ff70eb7c344d52e6041bcff57095530fc605c50d
gabrielloliveira <gabrielloliveira097@gmail.com>
2022-07-01 01:12:14 -0300

    feat: add Currency and Quote models

--- br_med/core/admin.py
+++ br_med/core/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from br_med.core.models import Currency, Quote
+from django.utils.translation import gettext_lazy as _
+
+
+@admin.register(Currency)
+class CurrencyAdmin(admin.ModelAdmin):
+    list_display = ["acronym"]
+    search_fields = ["acronym", "name", "symbol"]
+
+
+@admin.register(Quote)
+class QuoteAdmin(admin.ModelAdmin):
+    list_display = ["date", "acronym", "value"]
+    autocomplete_fields = ["currency"]
+
+    def acronym(self, obj):
+        return obj.currency.acronym
+
+    acronym.short_description = _("Moeda")
--- br_med/core/models.py
+++ br_med/core/models.py
@@ -1,3 +1,43 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class DefaultBaseModel(models.Model):
+    created_at = models.DateTimeField(_("criado em"), auto_now_add=True)
+    updated_at = models.DateTimeField(_("atualizado em"), auto_now=True)
+    uuid = models.UUIDField(_("UUID"), unique=True, editable=False, default=uuid.uuid4)
+
+    class Meta:
+        abstract = True
+
+
+class Currency(DefaultBaseModel):
+    name = models.CharField(_("moeda"), max_length=25)
+    symbol = models.CharField(_("símbolo"), max_length=25)
+    acronym = models.CharField(_("acrônimo"), max_length=3)
+
+    class Meta:
+        verbose_name = _("moeda")
+        verbose_name_plural = _("moedas")
+
+    def __str__(self):
+        return f"{self.acronym} - {self.name}"
+
+    def save(self, *args, **kwargs):
+        self.acronym = self.acronym.upper()
+        return super(Currency, self).save(*args, **kwargs)
+
+
+class Quote(DefaultBaseModel):
+    date = models.DateField(_("data de referência"))
+    currency = models.ForeignKey("core.Currency", on_delete=models.CASCADE)
+    value = models.DecimalField(_("valor da cotação"), max_digits=100, decimal_places=20)
+
+    class Meta:
+        verbose_name = _("cotação")
+        verbose_name_plural = _("cotações")
+
+    def __str__(self):
+        return f"{self.currency.acronym} - {self.date} - {self.value}"
